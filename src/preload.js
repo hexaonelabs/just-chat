720098ee6e6418c176b91baab99258ec2589ea03
FazioNico <contact@nicolasfazio.ch>
2024-09-09 15:43:58 +0200

    refactor: rmv unused file

--- src/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onRelayerAddresses: (callback) => ipcRenderer.on('relayer-addresses', (_event, value) => callback(value)),
-})
